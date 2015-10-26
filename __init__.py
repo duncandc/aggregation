cea4720777f4067b0ba25a3b2263b0dacc9f0f81
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-26 12:47:30 -0400

    first commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# This sub-module is destined for common non-package specific utility
+# functions that will ultimately be merged into `astropy.utils`
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+                        
